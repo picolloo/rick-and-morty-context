ed7810468c8e9f4ac8fabaff5ba9d59166443f89
Lucas Picollo <lucas.picollo@geekhunter.com.br>
2019-12-26 19:37:22 -0300

    favorite tool implemented

--- src/App.js
+++ src/App.js
@@ -1,17 +1,72 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 
 import { Store } from "./store";
+import "./App.css";
 
 function App() {
-  const store = useContext(Store);
+  const { state, dispatch } = useContext(Store);
 
-  console.log(store);
+  useEffect(() => {
+    const fetchEpisodes = async () => {
+      const data = await fetch(
+        "https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes"
+      );
+      const dataJSON = await data.json();
+
+      return dispatch({
+        type: "FETCH_DATA",
+        payload: dataJSON._embedded.episodes
+      });
+    };
+
+    fetchEpisodes();
+  }, []);
+
+  const handleToggleFavEvent = episode => {
+    if (!episode.fav) {
+      return dispatch({
+        type: "ADD_FAV",
+        payload: { id: episode.id }
+      });
+    }
+
+    dispatch({
+      type: "DEL_FAV",
+      payload: { id: episode.id }
+    });
+  };
 
   return (
     <div className="App">
-      <h1>Rick and morty</h1>
+      <div className="header">
+        <div>
+          <h2>Rick and morty</h2>
+          <span>Pick your favourites episodes:</span>
+        </div>
 
-      <p>Pick your favourites episodes:</p>
+        <span>Favourite(s): {state.episodes.filter(ep => ep.fav).length}</span>
+      </div>
+
+      <div>
+        <ul className="card-grid">
+          {state.episodes.map(ep => (
+            <li
+              key={ep.id}
+              className={`card-item ${!ep.fav && "fade"}`}
+              onClick={() => handleToggleFavEvent(ep)}
+            >
+              <img src={ep.image.medium} />
+              <div>{ep.name}</div>
+
+              <section>
+                <div>
+                  Season: {ep.season} Number: {ep.number}
+                </div>
+              </section>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,38 @@
-import React, { createContext } from "react";
+import React, { createContext, useReducer } from "react";
 
 export const Store = createContext();
 
-const initialState = {};
+const initialState = {
+  episodes: []
+};
 
-const reducer = () => {};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_FAV":
+      return {
+        ...state,
+        episodes: state.episodes.map(ep =>
+          ep.id === action.payload.id ? { ...ep, fav: true } : ep
+        )
+      };
+    case "DEL_FAV":
+      return {
+        ...state,
+        episodes: state.episodes.map(ep =>
+          ep.id === action.payload.id ? { ...ep, fav: null } : ep
+        )
+      };
+    case "FETCH_DATA":
+      return { ...state, episodes: action.payload };
+    default:
+      return state;
+  }
+};
 
-export function StoreProvider({ value, children }) {
-  return <Store.Provider value={value}>{children}</Store.Provider>;
+export function StoreProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <Store.Provider value={{ state, dispatch }}>{children}</Store.Provider>
+  );
 }
